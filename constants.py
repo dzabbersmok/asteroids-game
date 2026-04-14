cc2e3af743f798937d99801100591786097faf29
dzabbersmok <tyrnis@gmail.com>
2026-04-14 13:23:58 +0200

    add: draw player ship on screen!

--- constants.py
+++ constants.py
@@ -1,2 +1,9 @@
+# SCREEN
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+
+# PLAYER
+# the radius of the player's ship
+PLAYER_RADIUS = 20
+# the width of the lines that draw the player's ship
+LINE_WIDTH = 2
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-
+from player import Player
 
 
 def main():
@@ -14,6 +14,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     
     while True:
         log_state()
@@ -22,11 +26,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-            screen.fill("black")
-            pygame.display.flip()
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
 
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
-
+        
 if __name__ == "__main__":
     main()
